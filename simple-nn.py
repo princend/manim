7253d8d6ed06e7e0d58232a0ff7963e9c325fbc7
princend <waxdji3g4y3c04@gmail.com>
2024-11-26 11:40:38 +0800

    simple nn

--- simple-nn.py
+++ simple-nn.py
@@ -4,16 +4,27 @@ from manim import *
 class NN(Scene):
     def construct(self):
       
-        neuron_image = ImageMobject("assets/neuron.webp")
-        neuron_image.scale(0.5)
-        neuron_image.set_opacity()
-        self.add(neuron_image)
+        neuron_image = ImageMobject("assets/neuron.jpg")
+        neuron_image.scale(2)
+        neuron_text = Text("神經元 Neuron", font_size=60)
+        neuron_text.next_to(neuron_image, UP)
+        self.play(Write(neuron_text))
+        self.play(FadeIn(neuron_image))
+        self.wait(1)
+        self.play(FadeOut(neuron_text),neuron_image.animate.scale(0.2))
 
-        self.wait(2)
+
+        created_neuron_circle = Circle(radius=0.3,color=YELLOW,fill_color=BLACK,fill_opacity=0)
+        self.play(Create(created_neuron_circle.set_opacity(0.5)),neuron_image.animate.set_opacity(0),run_time=1)
+        self.remove(neuron_image)
+        self.play(created_neuron_circle.animate.set_opacity(1),run_time=0.5)
+
         # 創建神經元
-        input_layer = VGroup(*[Circle(radius=0.3) for _ in range(3)])
-        hidden_layer = VGroup(*[Circle(radius=0.3) for _ in range(4)])
-        output_layer = Circle(radius=0.3)
+        input_layer = VGroup(*[Circle(radius=0.3,color=YELLOW) for _ in range(3)])
+        # self.play(Create(input_layer[0].set_opacity(1)))
+        
+        hidden_layer = VGroup(*[Circle(radius=0.3,color=YELLOW) for _ in range(4)])
+        output_layer = Circle(radius=0.3,color=YELLOW)
 
         # 排列神經元位置
         input_layer.arrange(DOWN, buff=0.5)
@@ -23,7 +34,7 @@ class NN(Scene):
         input_layer.move_to(LEFT * 4)
         hidden_layer.move_to(LEFT * 0)
         output_layer.move_to(RIGHT * 4)
-
+        self.play(created_neuron_circle.animate.move_to(input_layer[0].get_center()))
         # 創建連接線
         connections1 = VGroup(*[
             Line(input_neuron.get_center(), hidden_neuron.get_center())
@@ -37,6 +48,20 @@ class NN(Scene):
         ])
 
         # 添加到場景
-        # self.add(input_layer, hidden_layer, output_layer)
-        # self.add(connections1, connections2)
-     
+        
+        self.play(Create(input_layer))
+        self.play(Create(hidden_layer))
+        self.play(Create(output_layer))
+        self.play(Create(connections1),Create(connections2))
+        self.wait(0.5)
+        input_text = Text("輸入層", font_size=36)
+        hidden_text = Text("隱藏層", font_size=36)
+        output_text = Text("輸出層", font_size=36)
+        input_text.next_to(input_layer, UP)
+        hidden_text.next_to(hidden_layer, UP)
+        output_text.next_to(output_layer, UP)
+        self.play(Write(input_text))
+        self.play(Write(hidden_text))
+        self.play(Write(output_text))
+        self.wait(1)
+
